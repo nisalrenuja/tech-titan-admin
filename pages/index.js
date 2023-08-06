6b37bb93e6df023694c51984867314aed27d27b2
nisalrenuja <nrenuja@gmail.com>
2023-08-06 21:46:38 +0530

    overview layout added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
 import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  return <Layout>test</Layout>;
+  const { data: session } = useSession();
+  return (
+    <Layout>
+      <div className="ml-4 flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}!</b>
+        </h2>
+        <div className="flex gap-1 bg-gray-300 rounded-l-full">
+          <img
+            src={session?.user?.image}
+            className="w-6 h-6 rounded-full"
+          ></img>
+          <span className="px-2">{session?.user?.name}</span>
+        </div>
+      </div>
+    </Layout>
+  );
 }
