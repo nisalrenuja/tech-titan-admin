6dd854a0e26606cdafa7a05bf16f15a3f007bc82
nisalrenuja <nrenuja@gmail.com>
2023-08-06 21:46:50 +0530

    products layout added

--- pages/products.js
+++ pages/products.js
@@ -1,5 +1,17 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Products() {
-  return <Layout>Product page here..</Layout>;
+  return (
+    <Layout>
+      <div className="mt-6">
+        <Link
+          href={`products/new`}
+          className="bg-black text-white py-4 px-4 rounded-md ml-4"
+        >
+          Add New Product
+        </Link>
+      </div>
+    </Layout>
+  );
 }
