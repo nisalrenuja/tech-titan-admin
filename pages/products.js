4bd52d9b891e6d2eef9af7092951144e4af3cef1
nisalrenuja <nrenuja@gmail.com>
2023-08-07 23:52:25 +0530

    product page get api added

--- pages/products.js
+++ pages/products.js
@@ -1,16 +1,62 @@
 import Layout from "@/components/Layout";
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/products").then((res) => {
+      setProducts(res.data);
+      console.log(res.data);
+    });
+  }, []);
   return (
     <Layout>
-      <div className="mt-6">
+      <div className="mt-6 mb-8">
         <Link
           href={`products/new`}
           className="bg-black text-white py-4 px-4 rounded-md ml-4"
         >
           Add New Product
         </Link>
+      </div>
+      <div className="mr-4 ml-4">
+        <table className="basic">
+          <thead>
+            <tr>
+              <td>Product Name</td>
+              <td></td>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr>
+                <td>{product.title}</td>
+                <td>
+                  <Link className="flex" href={"products/edit/" + product._id}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                      />
+                    </svg>
+                    Edit
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </Layout>
   );
