811acd0ce46bb2bc94f34476883aed0321321c63
nisalrenuja <nrenuja@gmail.com>
2023-08-07 23:52:35 +0530

    get api  fixes

--- pages/api/products.js
+++ pages/api/products.js
@@ -5,6 +5,16 @@ export default async function handler(req, res) {
   const { method } = req;
   await mongooseConnect();
   const { title, description, price } = req.body;
+
+  if (method === "GET") {
+    if (req.query?.id) {
+      res.json(await product.findOne({ _id: req.query.id }));
+    } else {
+      const products = await product.find({});
+      res.json(products);
+    }
+  }
+
   if (method === "POST") {
     const productDoc = await product.create({
       title,
