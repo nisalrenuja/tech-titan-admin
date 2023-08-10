1cc8cbf004e1a07b4146fde993faea904e51eeca
nisalrenuja <nrenuja@gmail.com>
2023-08-10 23:20:10 +0530

    product form remove

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,58 +1,11 @@
-import Layout from "@/components/Layout";
-import { useState } from "react";
-import axios from "axios";
-import { useRouter } from "next/router";
+import ProductForm from "../../components/ProductForm";
+import Layout from "../../components/Layout";
 
-export default function NewProducts() {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
-  const [goToProducts, setGoToProducts] = useState(false);
-  const router = useRouter();
-
-  async function createProduct(e) {
-    e.preventDefault();
-    const data = {
-      title,
-      description,
-      price,
-    };
-    axios.post("/api/products", data);
-    setGoToProducts(true);
-  }
-
-  if (goToProducts) {
-    router.push("/products");
-  }
-
+export default function NewProductPage() {
   return (
     <Layout>
-      <form onSubmit={createProduct}>
-        <div className="mt-4 ml-4 mr-4">
-          <h1>Add New Product</h1>
-          <label>Product Name</label>
-          <input
-            type="text"
-            placeholder="Product Name"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <label>Product Description</label>
-          <textarea
-            placeholder="Product Description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-          <label>Product Price</label>
-          <input
-            type="number"
-            placeholder="Price (in USD)"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-          />
-          <button className="btn-primary">Save</button>
-        </div>
-      </form>
+      <h1>Add New Product</h1>
+      <ProductForm />
     </Layout>
   );
 }
