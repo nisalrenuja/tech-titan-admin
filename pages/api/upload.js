0930c3b57f349b4768434e3670cb0877b0a75a6c
nisalrenuja <nrenuja@gmail.com>
2023-08-14 01:08:38 +0530

    cloudinary service added

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,4 +1,5 @@
 import multiparty from "multiparty";
+import { v2 as cloudinary } from "cloudinary";
 
 export default async function handler(req, res) {
   const form = new multiparty.Form();
@@ -8,6 +9,20 @@ export default async function handler(req, res) {
       resolve({ fields, files });
     });
   });
+  cloudinary.config({
+    cloud_name: "dsswfdxuq",
+    api_key: "765842844738341",
+    api_secret: "StHYORkAKNnd5L_wBMLrP_v4PyE",
+  });
+  try {
+    const result = await cloudinary.uploader.upload(files.file[0].path, {
+      folder: "tech-titan",
+    });
+
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error: "Image upload failed" });
+  }
   console.log("length:", files.file.length);
 }
 
